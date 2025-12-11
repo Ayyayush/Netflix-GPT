2b59011265ca9e715a6a19e2956f4f46117a64e8
Ayush <140370865+Ayyayush@users.noreply.github.com>
2025-12-11 11:47:12 +0530

    Sign-IN/UP

--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Header from "./Header";
+import { useState } from "react";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);          // usestate hook
+
+  const toggleSignInForm = () =>             // ye event handler hai 
+      {  
+    setIsSignInForm(!isSignInForm); // FIXED TYPO
+  };
+
   return (
     <div className="relative h-screen w-screen bg-black">
-      
       {/* Background Image */}
       <img
         src="https://singh-cp.github.io/netflix-landingpage/images/netflix-background-image.jpg"
@@ -19,23 +26,34 @@ const Login = () => {
       <Header />
 
       {/* Login Form */}
-      <form className="absolute top-1/2 left-1/2 w-[350px] -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-80 p-10 rounded-lg text-white">
-        <h1 className="text-3xl font-semibold mb-5">Sign In</h1>
+      <form className="absolute top-1/2 left-1/2 w-[350px] -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-60 p-10 rounded-lg text-white">
+        <h1 className="text-3xl font-bold mb-5">
+          {isSignInForm ? "Sign In" : "Sign Up"}        
+        </h1>
+
+        {/* Full Name only for Sign Up */}
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full name"
+            className="p-4 my-4 w-full mb-4 rounded bg-gray-700"
+          />
+        )}
 
         <input
           type="text"
           placeholder="Email or phone number"
-          className="p-3 w-full mb-4 rounded bg-gray-700"
+          className="p-4 my-4 w-full mb-4 rounded bg-gray-700"
         />
 
         <input
           type="password"
           placeholder="Password"
-          className="p-3 w-full mb-6 rounded bg-gray-700"
+          className="p-4 w-full my-6 mb-6 rounded bg-gray-700"
         />
 
-        <button className="w-full bg-red-600 p-3 rounded font-semibold hover:bg-red-700 duration-300">
-          Sign In
+        <button className="w-full bg-red-700 p-3 rounded font-semibold hover:bg-red-700 duration-300">
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
 
         <div className="flex justify-between text-sm text-gray-400 mt-3">
@@ -45,6 +63,12 @@ const Login = () => {
           </label>
           <a href="/">Need help?</a>
         </div>
+
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already registered? Sign In Now"}
+        </p>
       </form>
     </div>
   );
